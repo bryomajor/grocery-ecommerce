c7cbd828dec322519a90deb22a3cc7bb01cf0a71
bryomajor <bryomajor@gmail.com>
2020-12-04 23:24:36 +0300

    remove commented out code

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -5,13 +5,3 @@
 </div>
 {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
 {!! Form::close() !!}
-{{-- <form action="{{route('categories.store')}}" method="POST">
-    <div class="form-group">
-        {{csrf_field()}}
-        <div class="form-label">
-            Name:
-        </div>
-        <input type="text" class="form-control" placeholder="Category Name" name="category" id="category">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form> --}}
